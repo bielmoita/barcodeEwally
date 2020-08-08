550835f368039d6ddb4f2459e6762b5f71fd9fde
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-08-07 23:25:46 -0300

    Estrutura do projeto

---
+++ BACK/identificador/src/main/java/com/saibaseubarcode/identificador/IdentificadorBarCodeApplication.java
@@ -0,0 +1,13 @@
+package com.saibaseubarcode.identificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentificadorBarCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentificadorBarCodeApplication.class, args);
+	}
+
+}
---
+++ BACK/identificador/src/test/java/com/saibaseubarcode/identificador/IdentificadorBarCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saibaseubarcode.identificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentificadorBarCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
